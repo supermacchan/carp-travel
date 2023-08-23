80f604badd162125465810bd615b2f96a58cda34
Olga Birkovska <aiba.aki9@gmail.com>
2023-08-23 13:13:52 +0300

    about styles + hero styles adjustments + global styles

--- app/components/About/About.tsx
+++ app/components/About/About.tsx
@@ -1,25 +1,46 @@
+import { Overlay } from "../Overlay/Overlay"
+
 export const About = () => {
     return (
-        <section id="about">
-            {/* temp styles for visual readability */}
-            <h2 className="uppercase text-4xl">
-                Who we are
-            </h2>
-
-            <p> 
-                <span>a team of enthusiasts</span> who are fully committed to the mission of creating unforgettable and extraordinary trips to the most beautiful parts of the Carpathians. Our goal is not just to show you the natural wonders of the mountains, but to provide you with a deep immersion in their magical atmosphere.
-            </p>
-            <p>
-                <span>We believe</span> that nature has the power to inspire, strengthen character and provide new perspectives. Therefore, each of our tours is aimed at unlocking your potential, enriching your spiritual world and creating unforgettable memories.
-            </p>
-
-            <span>From vacationers</span>
-            <span>to active travelers</span>
-            <span>we have a tour for everyone.</span>
-
-            <p>
-                We use methods that are time-tested and proven. Our expert guides with in-depth knowledge of the Carpathian landscapes lead you safely through the mysteries of these mountains. 
-            </p>
+        <section 
+            id="about" 
+            className="bg-bg-about-mob bg-cover bg-no-repeat bg-center 
+                tab:bg-bg-about-tab desk:bg-bg-about-desk"
+        >
+            <Overlay>
+                <div className="section px-5 py-[55px] mx-auto text-white
+                    tab:px-8 tab:py-16 tab:h-[697px] tab:relative
+                    desk:px-6 desk:py-[104px] desk:h-[824px]">
+                    <h2 className="title mb-2">
+                        Who <span className="font-medium">we are</span>
+                    </h2>
+                    
+                    <div className="w-[180px] text-sm font-extralight leading-5
+                        tab:w-[221px] tab:absolute tab:top-16 tab:right-8 tab:text-base tab:leading-[20px]
+                        desk:w-[292px] desk:left-[654px] desk:top-[104px] desk:text-lg desk:leading-6">
+                        <p className="mb-5 tab:mb-4 desk:mb-6"> 
+                            <span className="font-normal">a team of enthusiasts</span> who are fully committed to the mission of creating unforgettable and extraordinary trips to the most beautiful parts of the Carpathians. Our goal is not just to show you the natural wonders of the mountains, but to provide you with a deep immersion in their magical atmosphere.
+                        </p>
+                        <p className="mb-10 tab:mb-0">
+                            <span className="font-normal">We believe</span> that nature has the power to inspire, strengthen character and provide new perspectives. Therefore, each of our tours is aimed at unlocking your potential, enriching your spiritual world and creating unforgettable memories.
+                        </p>
+                    </div>
+                    
+                    <div className="ml-auto mb-10 w-[180px] text-sm font-normal leading-5
+                        tab:w-[221px] tab:mb-0 tab:absolute tab:bottom-[188px] tab:left-8 tab:text-base tab:leading-[20px]
+                        desk:w-[296px] desk:bottom-[104px] desk:right-6 desk:text-lg desk:leading-6">
+                        <span className="block uppercase">From vacationers</span>
+                        <span className="block uppercase text-right">to active travelers</span>
+                        <span className="mr-[-15px] block font-extralight tracking-[-0.14px] tab:tracking-[0.32px] desk:mr-[-20px] desk:tracking-[2.16px]">we have a tour for everyone.</span>
+                    </div>
+                    
+                    <p className="text-sm font-extralight leading-5
+                        tab:w-[463px] tab:absolute tab:bottom-16 tab:right-8 tab:text-base tab:leading-[20px]
+                        desk:w-[605px] desk:bottom-[104px] desk:left-6 desk:text-lg desk:leading-6">
+                        <span className="font-medium">We use methods</span> that are time-tested and proven. Our expert guides with in-depth knowledge of the Carpathian landscapes lead you safely through the mysteries of these mountains. 
+                    </p>
+                    </div>
+            </Overlay>
         </section>
     )
 }
--- app/components/Hero/Hero.tsx
+++ app/components/Hero/Hero.tsx
@@ -2,16 +2,17 @@ import { Overlay } from "../Overlay/Overlay"
 
 export const Hero = () => {
     return (
-        <div 
+        <section 
             className="mt-[-105px] pt-[105px]
                 tab:mt-[-124px] tab:pt-[124px]
                 desk:mt-[-130px] desk:pt-[130px]
-                bg-bg-hero bg-cover bg-no-repeat bg-overlay"
+                bg-bg-hero-mob bg-cover bg-no-repeat bg-center 
+                tab:bg-bg-hero-tab desk:bg-bg-hero-desk"
         >
             <Overlay>
-                <section className="px-5 pb-14 mx-auto max-w-screen-lg-mob text-white
-                    tab:px-8 tab:pb-16 tab:h-[410px] tab:max-w-screen-tab tab:w-[768px] tab:relative
-                    desk:px-6 desk:pb-20 desk:w-[1280px] desk:h-[577px] desk:max-w-screen-desk">
+                <div className="section px-5 pb-14 mx-auto
+                    tab:px-8 tab:pb-16 tab:h-[410px] tab:relative
+                    desk:px-6 desk:pb-20 desk:h-[577px]">
 
                     <h1 className="ml-auto mb-6 flex flex-wrap justify-end w-[123px] text-right uppercase
                         tab:w-[230px] tab:absolute tab:right-8 tab:top-0 desk:w-[305px] desk:right-6">
@@ -20,15 +21,15 @@ export const Hero = () => {
                         <span className="relative right-[-5px] text-xs font-light tracking-[9.48px] leading-3 tab:right-[-20px] tab:text-sm tab:leading-3 tab:tracking-[25.9px] desk:right-[-30px] desk:text-base desk:leading-4 desk:tracking-[36.48px]">journey</span>
                     </h1>
 
-                    <h2 className="mb-6 w-[280px] flex flex-col text-4xl text-[40px] font-thin tracking-[-1.6px] leading-[56px] uppercase
-                        tab:w-[426px] tab:absolute tab:left-8 tab:top-0 tab:text-[67px] tab:tracking-[-2.68px] tab:leading-[67px]
-                        desk:w-[646px] desk:left-6 desk:text-[98px] desk:tracking-[-3.92px] desk:leading-[98px]">
+                    <h2 className="title mb-6 w-[280px] flex flex-col 
+                        tab:w-[426px] tab:absolute tab:left-8 tab:top-0
+                        desk:w-[646px] desk:left-6">
                         <span className="font-medium">Uncover</span>
                         Carpathian’s Secrets
                     </h2>
 
                     <p className="mb-6 w-[165px] text-[10px] font-extralight leading-4
-                        tab:mb-0 tab:w-[270px] tab:absolute tab:bottom-16 tab:left-8 tab:text-sm tab:tracking-[1.26px]
+                        tab:mb-0 tab:w-[270px] tab:absolute tab:bottom-16 tab:left-8 tab:text-sm tab:tracking-[1.26px] tab:leading-[16px]
                         desk:w-auto desk:bottom-20 desk:left-6 desk:text-base desk:leading-6 desk:tracking-[1.44px]">
                         Hoverla / Yaremche / Zakarpattia / Vorokhta / Synevyr Lake / Bukovel
                     </p>
@@ -36,7 +37,7 @@ export const Hero = () => {
                     <div className="w-[280px] tab:w-[230px] tab:absolute tab:bottom-16 tab:right-8
                         desk:w-[294px] desk:bottom-20 desk:right-6">
                         <p className="mb-6 text-sm font-extralight leading-[20px] text-justify
-                            tab:mb-7 tab:text-base desk:text-lg desk:leading-6">
+                            tab:mb-7 tab:text-base tab:leading-[20px] desk:text-lg desk:leading-6">
                             We offer you unforgettable trips to the most beautiful parts of the Carpathians. Enjoy stunning views, exciting expeditions, and the best service!
                         </p>
 
@@ -55,8 +56,8 @@ export const Hero = () => {
                             Join now
                         </button>
                     </div>
-                </section>
+                </div>
             </Overlay>
-        </div>
+        </section>
     )
 }
